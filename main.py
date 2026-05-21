280d727143897c751781dcc534c43ac2f278c1ea
JoaoVitorMallmannBorges <2025009485@aluno.restinga.ifrs.edu.br>
2026-05-21 10:38:52 -0300

    versão final do programa

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from random import randint
+import random 
+import copy
 
 class Processo:
-    def __init__(self, tempo_exec, tempo_chegada, prioridade):
+    def __init__(self, posicao, tempo_exec, tempo_chegada, prioridade):
         self.tempo_exec = tempo_exec
         self.tempo_chegada = tempo_chegada
         self.tempo_restante = tempo_exec
         self.prioridade = prioridade
+        self.posicao = posicao
 
-def cria_processo_aleatorio():
-    return Processo(randint(1, 12), randint(2, 15), randint(1, 20))
+def cria_processo_aleatorio(posicao):
+    return Processo(posicao, random.randint(1, 12), random.randint(2, 15), random.randint(1, 20))
 
 def mostrar_processos(lista_processo): 
     print("\n--- Lista de Processos ---")
@@ -25,21 +27,28 @@ def formatar_media(media):
 
 
 lista_processos = []
-total_processos = 5
+total_processos = 3
+posicao = 0
 
-opcao = input('Os parâmetros serão aleatórios? (S/N): ').upper()
+while len(lista_processos) != total_processos:
 
-if opcao == 'S':
-    for x in range(total_processos):
-        lista_processos.append(cria_processo_aleatorio())
-else:
-    print("Insira os dados manualmente:")
-    for i in range(total_processos):
-        print(f"\nDados do Processo {i}:")
-        t_exec = int(input("Tempo de execução: "))
-        t_cheg = int(input("Tempo de chegada: "))
-        prio = int(input("Prioridade: "))
-        lista_processos.append(Processo(t_exec, t_cheg, prio))
+    opcao = input('Os parâmetros serão aleatórios? (S/N): ').upper()
+
+    if opcao == 'S':
+        for x in range(total_processos):
+            lista_processos.append(cria_processo_aleatorio(posicao))
+            posicao += 1
+    elif opcao == 'N':
+        print("Insira os dados manualmente:")
+        for i in range(total_processos):
+            print(f"\nDados do Processo {i}:")
+            t_exec = int(input("Tempo de execução: "))
+            t_cheg = int(input("Tempo de chegada: "))
+            prio = int(input("Prioridade: "))
+            lista_processos.append(Processo(posicao, t_exec, t_cheg, prio))
+            posicao += 1 
+    else:
+        print(f'A escola e apenas de S ou N, define certo ai')
 
 mostrar_processos(lista_processos)
 
@@ -78,17 +87,245 @@ while flag:
         print(f'tempo restante média: {media}')
         print('--------------------------')
 
-    if opcao_2 == '7':
+    elif opcao_2 =='2':
+        lista_copia = copy.deepcopy(lista_processos)
+        tempo = 1
+        lista_espera = []
+        lista_espera_soma = []
+
+        while lista_copia !=[]:
+            disponivel = []
+            for x in lista_copia:
+                if x.tempo_chegada <= tempo:
+                    disponivel.append(x)
+            if disponivel == []:
+                print(f'Tempo[{tempo}]:ainda não está pronto')
+                tempo += 1
+            else:
+                disponivel.sort(key=lambda p: p.tempo_restante)
+                processo = disponivel.pop(0)
+                lista_copia.remove(processo)
+                processo.tempo_restante -= 1
+                print(f'tempo[{tempo}]: processo[{processo.posicao}] restante[{processo.tempo_restante}]')
+                tempo += 1
+                if processo.tempo_restante==0:
+                    tempo_atual = tempo
+                    tempo_retorno = tempo_atual - processo.tempo_chegada
+                    tempo_espera = tempo_retorno - processo.tempo_exec
+                    espera = (processo.posicao, tempo_espera)
+                    lista_espera.append(espera)
+                    lista_espera_soma.append(tempo_espera)
+                else:
+                    lista_copia.append(processo)
+        print('--------------------------')
+        lista_espera.sort()
+        for e in range(len(lista_espera)):
+            print(f'processo[{lista_espera[e][0]}]: tempo de espera = {lista_espera[e][1]}')
+        print('--------------------------') 
+        soma = sum(lista_espera_soma)
+        if soma > 0:
+            media = soma/len(lista_espera_soma)
+            media = formatar_media(media)
+            print(f'tempo restante média: {media}')
+            print('--------------------------')   
+        else:
+            print(f'tempo restante média: {soma}')
+            print('--------------------------') 
+            
+
+
+
+            
+    
+    elif opcao_2 == '3':
+        
+        lista_copia = copy.deepcopy(lista_processos)
+        tempo = 0
+        tempo_simulado = 0
+        lista_espera = []
+        lista_espera_soma = []
+        lista_organizada = []
+
+        while lista_copia !=[]:
+            disponivel = []
+            for x in lista_copia:
+                if x.tempo_chegada <= tempo_simulado:
+                    disponivel.append(x)
+            if disponivel == []:
+                tempo_simulado += 1 
+            else:
+                disponivel.sort(key=lambda p: p.tempo_restante)
+                processo = disponivel.pop(0)
+                tempo_simulado += processo.tempo_exec
+                lista_organizada.append(processo)
+                lista_copia.remove(processo)
+
+                tempo_atual = tempo_simulado
+                tempo_retorno = tempo_atual - processo.tempo_chegada
+                tempo_espera = tempo_retorno - processo.tempo_exec
+                espera = (processo.posicao, tempo_espera)
+                lista_espera.append(espera)
+                lista_espera_soma.append(tempo_espera)
+        
+        for p in range(len(lista_organizada)):
+            tempo_execucao = 0
+            if lista_organizada[p].tempo_chegada > tempo:
+                for t in range((lista_organizada[p].tempo_chegada - tempo)-1):
+                    tempo += 1
+                    print(f'tempo[{tempo}]: ainda não está pronto')
+            for t in range(lista_organizada[p].tempo_exec):
+                tempo += 1
+                tempo_execucao += 1 
+                print(f'tempo[{tempo}]: processo[{lista_organizada[p].posicao}] restante[{lista_organizada[p].tempo_restante - (tempo_execucao)}]')
+        print('--------------------------')
+        lista_espera.sort()
+        for e in range(len(lista_espera)):
+            print(f'processo[{lista_espera[e][0]}]: tempo de espera = {lista_espera[e][1]}')
+        print('--------------------------') 
+        soma = sum(lista_espera_soma)
+        if soma > 0:
+            media = soma/len(lista_espera_soma)
+            media = formatar_media(media)
+            print(f'tempo restante média: {media}')
+            print('--------------------------')   
+        else:
+            print(f'tempo restante média: {soma}')
+            print('--------------------------') 
+                   
+            
+    elif opcao_2 == '4':
+        lista_copia = copy.deepcopy(lista_processos)
+        tempo = 1
+        lista_espera = []
+        lista_espera_soma = []
+
+        while lista_copia !=[]:
+            disponivel = []
+            for x in lista_copia:
+                if x.tempo_chegada <= tempo:
+                    disponivel.append(x)
+            if disponivel == []:
+                print(f'Tempo[{tempo}]:ainda não está pronto')
+                tempo += 1
+            else:
+                disponivel.sort(key=lambda p: (-p.prioridade, p.tempo_exec))
+                processo = disponivel.pop(0)
+                lista_copia.remove(processo)
+                processo.tempo_restante -= 1
+                print(f'tempo[{tempo}]: processo[{processo.posicao}] restante[{processo.tempo_restante}]')
+                tempo += 1
+                if processo.tempo_restante==0:
+                    tempo_atual = tempo
+                    tempo_retorno = tempo_atual - processo.tempo_chegada
+                    tempo_espera = tempo_retorno - processo.tempo_exec
+                    espera = (processo.posicao, tempo_espera)
+                    lista_espera.append(espera)
+                    lista_espera_soma.append(tempo_espera)
+                else:
+                    lista_copia.append(processo)
+        print('--------------------------')
+        lista_espera.sort()
+        for e in range(len(lista_espera)):
+            print(f'processo[{lista_espera[e][0]}]: tempo de espera = {lista_espera[e][1]}')
+        print('--------------------------') 
+        soma = sum(lista_espera_soma)
+        if soma > 0:
+            media = soma/len(lista_espera_soma)
+            media = formatar_media(media)
+            print(f'tempo restante média: {media}')
+            print('--------------------------')   
+        else:
+            print(f'tempo restante média: {soma}')
+            print('--------------------------') 
+            
+    elif opcao_2 == '5':
+        
+        lista_copia = copy.deepcopy(lista_processos)
+        tempo = 0
+        tempo_simulado = 0
+        lista_espera = []
+        lista_espera_soma = []
+        lista_organizada = []
+
+        while lista_copia !=[]:
+            disponivel = []
+            for x in lista_copia:
+                if x.tempo_chegada <= tempo_simulado:
+                    disponivel.append(x)
+            if disponivel == []:
+                tempo_simulado += 1 
+            else:
+                disponivel.sort(key=lambda p: (-p.prioridade, p.tempo_exec))
+                processo = disponivel.pop(0)
+                tempo_simulado += processo.tempo_exec
+                lista_organizada.append(processo)
+                lista_copia.remove(processo)
+
+                tempo_atual = tempo_simulado
+                tempo_retorno = tempo_atual - processo.tempo_chegada
+                tempo_espera = tempo_retorno - processo.tempo_exec
+                espera = (processo.posicao, tempo_espera)
+                lista_espera.append(espera)
+                lista_espera_soma.append(tempo_espera)
+        
+        for p in range(len(lista_organizada)):
+            tempo_execucao = 0
+            if lista_organizada[p].tempo_chegada > tempo:
+                for t in range((lista_organizada[p].tempo_chegada - tempo)-1):
+                    tempo += 1
+                    print(f'tempo[{tempo}]: ainda não está pronto')
+            for t in range(lista_organizada[p].tempo_exec):
+                tempo += 1
+                tempo_execucao += 1 
+                print(f'tempo[{tempo}]: processo[{lista_organizada[p].posicao}] restante[{lista_organizada[p].tempo_restante - (tempo_execucao)}]')
+        print('--------------------------')
+        lista_espera.sort()
+        for e in range(len(lista_espera)):
+            print(f'processo[{lista_espera[e][0]}]: tempo de espera = {lista_espera[e][1]}')
+        print('--------------------------') 
+        soma = sum(lista_espera_soma)
+        if soma > 0:
+            media = soma/len(lista_espera_soma)
+            media = formatar_media(media)
+            print(f'tempo restante média: {media}')
+            print('--------------------------')   
+        else:
+            print(f'tempo restante média: {soma}')
+            print('--------------------------') 
+
+    elif opcao_2 == '6':
+        tempo = 0
+        pedaco_tempo = int(input('Defina o time slice: '))
+        lista_processos_copia = []
+        lista_processos_copia = copy.deepcopy(lista_processos)
+        while lista_processos_copia != []:
+            processo = lista_processos_copia.pop(0)
+            if processo.tempo_restante >= pedaco_tempo:
+                for t in range(pedaco_tempo):
+                    tempo += 1
+                    processo.tempo_restante -= 1
+                    print(f'tempo[{tempo}]: processo[{processo.posicao}] restante={processo.tempo_restante}')
+                if processo.tempo_restante > 0:
+                    lista_processos_copia.append(processo)
+            else:
+                for t in range(processo.tempo_restante):
+                    tempo += 1
+                    processo.tempo_restante -= 1
+                    print(f'tempo[{tempo}]: processo[{processo.posicao}] restante={processo.tempo_restante}')
+
+
+    elif opcao_2 == '7':
         mostrar_processos
 
-    if opcao_2 =='8':
+    elif opcao_2 =='8':
         opcao = input('Os parâmetros serão aleatórios? (S/N): ').upper()
-
+        posicao = 0
         if opcao == 'S':
             
             lista_processos.clear()
             for x in range(total_processos):
-                lista_processos.append(cria_processo_aleatorio())
+                lista_processos.append(cria_processo_aleatorio(posicao))
+                posicao += 1 
 
        
         elif opcao == 'N':
@@ -100,14 +337,16 @@ while flag:
                 t_exec = int(input("Tempo de execução: "))
                 t_cheg = int(input("Tempo de chegada: "))
                 prio = int(input("Prioridade: "))
-                lista_processos.append(Processo(t_exec, t_cheg, prio))
+                lista_processos.append(Processo(posicao, t_exec, t_cheg, prio))
+                posicao += 1
         
         mostrar_processos(lista_processos)
     
-    if opcao_2 =='9':
+    elif opcao_2 =='9':
         print('Saindo...')
         flag = False 
-
+    else:
+        print('Erro tente novamente.')
 
         
 
